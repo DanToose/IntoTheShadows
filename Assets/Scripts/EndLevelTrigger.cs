ae61e0cefb27521a76bfa341f6fc414564352794
Dan Toose <dantoose@gmail.com>
2022-07-14 16:03:15 +1000

    End Panel Now Triggers with coin tally
    As above.
    
    Also hooked up correct sounds, but not firing on being spotted.

--- Assets/Scripts/EndLevelTrigger.cs
+++ Assets/Scripts/EndLevelTrigger.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndLevelTrigger : MonoBehaviour
 {
+    private GameObject player;
+    private GameObject EndGameCanvas;
+    private int coins;
+    private int coinsTotal;
+    public Text coinsFound; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        EndGameCanvas = GameObject.Find("End_level_Canvas");
+        EndGameCanvas.SetActive(false);
     }
 
     // Update is called once per frame
-    void Update()
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "Player")
+        {
+            coins = player.GetComponent<PlayerInventory>().coinCount;
+            coinsTotal = player.GetComponent<PlayerInventory>().coinsToFind;
+            EndGameCanvas.SetActive(true);
+            coinsFound.text = coins + "/" + coinsTotal;
+            //player.SetActive(false);
+        }
+
+
     }
 }
